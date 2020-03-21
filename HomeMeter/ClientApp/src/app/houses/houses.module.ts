8d73a40975be2af8ed9e6c68775b9eaada8a2ceb
Websevee <Websevee@gmail.com>
2020-03-21 23:42:23 +0300

    New

---
+++ HomeMeter/ClientApp/src/app/houses/houses.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousesListComponent } from './houses-list/houses-list.component';
+
+
+
+@NgModule({
+  declarations: [HousesListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HousesModule { }
